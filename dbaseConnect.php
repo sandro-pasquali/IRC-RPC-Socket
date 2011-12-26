c4e3f54a50c0551084e717cb1fb51fa5e7880e21
Sandro Pasquali <spasquali@gmail.com>
2011-12-26 13:42:45 -0500

    updates

--- dbaseConnect.php
+++ dbaseConnect.php
@@ -4,8 +4,8 @@
 
 $DB_USER = "phpread";
 $DB_PASS = "gn0fp51";
-$DB_NAME = "datecam";
-$DB_ADDRESS = "69.36.37.5:3306"; 
+$DB_NAME = "NAME";
+$DB_ADDRESS = "1.1.1.1:3306"; 
 
 if(mysql_pconnect($DB_ADDRESS,$DB_USER,$DB_PASS))
   {
